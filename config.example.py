e25ee28b949af535b7807f7c7e6b35b1b5b914c3
Maksim <wolker2015@gmail.com>
2024-09-14 23:19:34 +0700

    fixed config

--- config.example.py
+++
@@ -1,3 +0,0 @@
-API_ID = 000
-API_HASH = 'хеш'
-BOT_TOKEN = 'токен'
--- http_client.py
+++ http_client.py
@@ -5,14 +5,20 @@ from models import User, Rating
 
 class HttpClient:
     def __init__(self):
-        self.session = aiohttp.ClientSession()
-        self.host = "https://f18e4325-89f0-4357-b6a4-2221454d52fb-00-24sehfqtbpqxn.janeway.replit.dev/"
+        self.session = None
+        self.host = "https://f18e4325-89f0-4357-b6a4-2221454d52fb-00-24sehfqtbpqxn.janeway.replit.dev"
         self.cache = {}
 
-    async def get_user(self, tg_id: int) -> User | None:
+    async def _get_session(self):
+        if self.session is None:
+            self.session = aiohttp.ClientSession()
+        return self.session
+
+    async def get_user(self, tg_id: int):
+        session = await self._get_session()
         if tg_id in self.cache:
             return self.cache[tg_id]
-        async with self.session.get(f"{self.host}/get_user?tg_id={tg_id}") as response:
+        async with session.get(f"{self.host}/get_user?tg_id={tg_id}") as response:
             if response.status == 200:
                 data = await response.json(content_type=None)
                 if data["status"] == 200:
@@ -21,9 +27,10 @@ class HttpClient:
                     return user
             return None
 
-    async def create_user(self, tg_id: int, username: str) -> User | None:
+    async def create_user(self, tg_id: int, username: str):
+        session = await self._get_session()
         url = f"{self.host}/create_user?tg_id={tg_id}&username={username}"
-        async with self.session.get(url) as response:
+        async with session.get(url) as response:
             if response.status == 200:
                 data = await response.json(content_type=None)
                 if data["status"] == 200:
@@ -32,14 +39,15 @@ class HttpClient:
                     return user
             return None
 
-    async def get_rating(self) -> list[Rating] | None:
-        async with self.session.get(f"{self.host}/get_rating") as response:
+    async def get_rating(self):
+        session = await self._get_session()
+        async with session.get(f"{self.host}/get_rating") as response:
             if response.status == 200:
                 data = await response.json(content_type=None)
                 if data["status"] == 200:
                     return [Rating(**rating) for rating in data["body"]["rating"]]
-
             return None
 
     async def close_session(self):
-        await self.session.close()
+        if self.session is not None:
+            await self.session.close()
